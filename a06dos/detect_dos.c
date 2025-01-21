5576e581b8a564a32327719a2765987aa89b8f6f
plyr0 <9119209+plyr0@users.noreply.github.com>
2025-01-21 12:03:22 +0100

    dos

--- a06dos/detect_dos.c
+++ a06dos/detect_dos.c
@@ -2,9 +2,8 @@
 https://forums.atariage.com/topic/186973-current-disk-device-number/
 https://forums.atariage.com/topic/252474-how-to-detect-sparta-dos/#comment-3507849
 http://atariki.krap.pl/index.php/Programowanie:_Jak_stwierdzi%C4%87_obecno%C5%9B%C4%87_SpartaDOS_X_oraz_jego_konfiguracj%C4%99_pami%C4%99ci
+C:\cc65\libsrc\atari\dosdetect.s
 */
-
-/* TODO: C:\cc65\libsrc\atari\dosdetect.s */
 
 #include <stdio.h>
 #include <atari.h>
@@ -13,11 +12,12 @@ http://atariki.krap.pl/index.php/Programowanie:_Jak_stwierdzi%C4%87_obecno%C5%9B
 
 #define DDEVIC          0x0300
 #define DUNIT           0x0301
-#define SPARTADOS_MAGIC 0x0700
-#define SPARTA_VERSION  0x0701
+#define DOS 			0x0700
+#define SDX_VERSION	    0x0701
+#define SDX_SUBVERSION	0x0702
 #define BEWEDOS_MAGIC   0x0703
 #define SPARTA_FILE_ID  0x0760
-#define SPARTA_DEVICE   0x0761
+#define SDX_DEVICE   	0x0761
 #define SPARTA_FILENAME 0x0762
 #define SPARTA_NAMELEN  ((uint8_t)11)
 
@@ -28,13 +28,19 @@ void sparta(void);
 int main(void)
 {
 	printf("\n");
-	c = PEEK(SPARTADOS_MAGIC);
+	c = PEEK(DOS);
 	if (c == 'S')
 		sparta();
 	else if (c == '\x0B' || c == '\xE8')
 		printf("Very old SpartaDOS 0x0700=%#x\n", c);
+	else if (c == 'M')
+		printf("MyDOS 0x0700=%#x\n", c);
+	else if (c == 'X')
+		printf("XDOS 0x0700=%#x\n", c);
+	else if (c == 'R')
+		printf("RealDOS 0x0700=%#x\n", c);
 	else
-		printf("No SpartaDOS 0x0700=%c\n", c);
+		printf("Unknown DOS 0x0700=%c\n", c);
 
 	printf("\n");
 	c = PEEK(DDEVIC);
@@ -50,15 +56,14 @@ int main(void)
 
 void sparta(void)
 {
-	printf("SpartaDOS 0x0700=%c\n", c);
-
 	c = PEEK(BEWEDOS_MAGIC);
-	if (c == 'B') {
+	if (c == 'B' && PEEK(BEWEDOS_MAGIC+1) == 'W') {
 		printf("BeweDOS 0x0703=%c\n", c);
 		return;
 	}
 
-	c = PEEK(SPARTA_VERSION);
+	printf("SpartaDOS 0x0700=%c\n", c);
+	c = PEEK(SDX_VERSION);
 	printf("Version 0x0701=%#x", c);
 
 	if (c < 0x41)
@@ -74,7 +79,7 @@ void sparta(void)
 	}
 	printf("\n");
 
-	c = PEEK(SPARTA_DEVICE);
+	c = PEEK(SDX_DEVICE);
 	printf("DEVICE 0x0761=%#x => ", c);
 	switch (c & 0b11110000) {
 	case 0: printf("DSK "); break;
